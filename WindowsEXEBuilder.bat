1e4263cdea27565908e555d1b9c0e4045c215db3
Macros Meng <Meng-yc@outlook.com>
2025-05-11 14:44:33 +0800

    Add builder for Windows™

---
+++ WindowsEXEBuilder.bat
@@ -0,0 +1,13 @@
+pyinstaller HexoGUI.py ^
+            --onefile ^
+            -n HexoGUI ^
+            --add-data "./HarmonyOS_Sans_SC_Regular.ttf:." ^
+            --add-data "JetBrainsMapleMono-Regular.ttf:." ^
+            --add-data "./ICONs.ico:." ^
+            --collect-all pyglet ^
+            --collect-all sv_ttk ^
+            --collect-all darkdetect ^
+            --splash "loading.png" ^
+            --noconsole ^
+            --icon ICONs.ico
+@pause
